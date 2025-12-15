ebcd7470c37288c3d9e32471d95fc66d26f29e92
Anjalipatil2407 <anjalipatil.24077@gmail.com>
2025-12-15 19:13:26 +0530

    Added 3 beginner-friendly string examples

---
+++ strings.py
@@ -0,0 +1,16 @@
+#Example 1 Accessing characters (Indexing)
+text = "Python"
+print(text[0])   # First character: P
+print(text[-1])  # Last character: n
+
+#Example 2 Slicing strings
+word = "Programming"
+print(word[0:6])   # 'Progra'
+print(word[3:])    # 'gramming'
+print(word[-6:-1]) # 'ammin'
+
+#Example 3 String repetition & concatenation
+a = "Hi"
+b = "There"
+print(a + " " + b)  # 'Hi There'
+print(a * 4)        # 'HiHiHiHi'
