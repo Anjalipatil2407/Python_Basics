b0f3b67ea34c95376c8a8efb1be64058346aca01
Anjalipatil2407 <anjalipatil.24077@gmail.com>
2025-12-15 19:22:34 +0530

    Added examples for list
    Added examples of list creation, printing, and modification.

---
+++ list.py
@@ -0,0 +1,18 @@
+#  Example-1 Creating and printing a list
+fruits = ["apple", "banana", "cherry", "orange"]
+
+print("List of Fruits:")
+print(fruits)
+print("Type:", type(fruits))
+
+# Example-2 Adding and removing elements in a list
+numbers = [1, 2, 3, 4]
+
+# Add an element
+numbers.append(5)
+
+# Remove an element
+numbers.remove(2)
+
+print("Updated Numbers List:")
+print(numbers)
