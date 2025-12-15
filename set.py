681aebe0c6c48f5eba18b90ecd8439492af24ff1
Anjalipatil2407 <anjalipatil.24077@gmail.com>
2025-12-15 19:34:08 +0530

    Create set.py to demonstrate set operations

---
+++ set.py
@@ -0,0 +1,18 @@
+# Creating and printing a set
+numbers = {1, 2, 3, 4, 5}
+
+print("Numbers Set:")
+print(numbers)
+print("Type:", type(numbers))
+
+# Adding and removing elements in a set
+fruits = {"apple", "banana", "cherry"}
+
+# Add element
+fruits.add("orange")
+
+# Remove element
+fruits.remove("banana")
+
+print("Updated Fruits Set:")
+print(fruits)
