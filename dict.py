817bee4cbef80010f9cf65612658bd1c4c5eaaa7
Anjalipatil2407 <anjalipatil.24077@gmail.com>
2025-12-15 19:33:10 +0530

    Added Dictionary examples

---
+++ dict.py
@@ -0,0 +1,17 @@
+# Creating and printing a dictionary for student info
+student = {"name": "Anita", "age": 19, "course": "Python"}
+
+print("Student Info:")
+print(student)
+print("Type:", type(student))
+
+# Updating and adding items in a dictionary
+car = {"brand": "Honda", "year": 2020}
+
+# Add new key-value
+car["color"] = "Red"
+
+# Update value
+car["year"] = 2022
+
+print("Updated Car Information:", car)
