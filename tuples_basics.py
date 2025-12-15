856ebb580cfde30f103f46d71f407778d4015399
Anjalipatil2407 <anjalipatil.24077@gmail.com>
2025-12-15 19:30:38 +0530

    Create tuples for fruits and colours
    Added a tuple of fruits and demonstrated basic operations on a tuple of colors.

---
+++ tuples_basics.py
@@ -0,0 +1,12 @@
+# Creating and printing a collection of fruits
+fruits = ("apple", "banana", "cherry", "orange")
+
+print("Fruits Collection:")
+print(fruits)
+print("Type:", type(fruits))
+
+# Basic operations on a collection of colors
+colors = ("red", "green", "blue")
+
+print("Length of colors collection:", len(colors))
+print("Is 'green' in the collection?", "green" in colors)
