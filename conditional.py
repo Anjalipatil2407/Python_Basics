752237c4f6db8f5ca9429191f0c22be662fea52c
Anjalipatil2407 <anjalipatil.24077@gmail.com>
2025-12-15 19:19:25 +0530

    Added conditional statements examples

---
+++ conditional.py
@@ -0,0 +1,28 @@
+#if statement
+# Executes a block of code only if the condition is true.
+age = 18
+if age >= 18:
+    print("You are eligible to vote")
+
+
+#if-else statement
+#Executes one block if the condition is true, another if it’s false.
+age = 16
+if age >= 18:
+    print("You can vote")
+else:
+    print("You are too young to vote")
+
+#elif (else if) statement
+#Checks multiple conditions in order.
+marks = 75
+if marks >= 90:
+    print("Grade: A+")
+elif marks >= 75:
+    print("Grade: A")
+elif marks >= 50:
+    print("Grade: B")
+else:
+    print("Grade: C")
+
+
