2f89a0835648968ec785f3b4d478c01bfeec59ed
Anjalipatil2407 <anjalipatil.24077@gmail.com>
2025-12-15 18:44:38 +0530

    Added variables program

---
+++ variables.py
@@ -0,0 +1,20 @@
+#Example 1 Total price calculation
+price = 10
+quantity = 3
+total = price*quantity
+print(total)
+
+#Example 2 Basic variables
+x = 10
+y = 5
+name = "Anjali"
+
+print(x)
+print(y)
+print(name)
+
+#Example 3 Addition using variables
+num1 = 10
+num2 = 6
+sum_result = num1 +num2
+print("Sum:",sum_result)
